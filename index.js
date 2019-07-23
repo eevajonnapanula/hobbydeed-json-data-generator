fd88d16b08f2e61d981e8b0908e78be7d78a4097
eevajonnapanula <eeva-jonna.panula@codento.com>
2019-07-23 14:18:25 +0300

    Fix for deployment

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const { ApolloServer } = require('apollo-server')
 const typeDefs = require('./typeDefs')
 const resolvers = require('./resolvers')
+require('dotenv').config()
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  playground: true,
 })
 
-server.listen().then(({ url }) => {
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`We are running in ${url}`)
 })
